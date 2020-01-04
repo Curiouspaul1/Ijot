1879e0520e865dc79621bafd69f49db297b64e5a
Curious Paul <170408025@live.unilag.edu.ng>
2020-01-04 01:49:34 +0100

    updated script

--- ijot1/__init__.py
+++ ijot1/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate,MigrateCommand
-from flask_script import Manager
 import os
 
 app = Flask(__name__)
