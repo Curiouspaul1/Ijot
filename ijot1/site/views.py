1a5b8839cdb207a9bf31a21d7cda42fc211681e9
Curious Paul <170408025@live.unilag.edu.ng>
2020-04-22 22:33:49 +0100

    fixed auth issues

--- ijot1/site/views.py
+++ ijot1/site/views.py
@@ -1,23 +1,37 @@
 from flask import render_template, url_for, request,g,redirect,flash,session
 from ijot1 import db,ma
 from ijot1.models import  User,Notes,note_schema
+from functools import wraps
 from . import sitemod
 import os
 
 
-@sitemod.before_request
+"""@sitemod.before_request
 def before_request():
 	g.user = None
 	if 'user' in session:
-		g.user = session['user']
+		g.user = session['user']"""
 
-@sitemod.route('/index') 
+def login_required(f):
+	@wraps(f)
+	def function(*args,**kwargs):
+		g.user = None
+		if 'user' in session:
+			g.user = session['user']
+			current_user = g.user
+			return f(current_user,*args,**kwargs)
+		else:
+			session['error'] = "Log in to access this service"
+			return redirect(url_for('site.signin'))
+	return function
+
+@sitemod.route('/')
+@sitemod.route('/index')
 def index():
 	return render_template('index.html')
 
 @sitemod.route('/signup',methods=['GET','POST'])
 def signup():
-	session.pop('user',None)
 	if request.method == 'POST':
 		# fetch data 
 		name = request.form['name']
@@ -34,7 +48,6 @@ def signup():
 @sitemod.route('/signin',methods=['GET','POST'])
 def signin():
 	if request.method == 'POST':
-		session.pop('user',None)
 		email = request.form['email']
 		user = User.query.filter_by(email=email).first()
 		if user != None:
@@ -49,11 +62,14 @@ def signin():
 		else:
 			error = 'No user with that email was found'
 			return render_template('signup.html',error=error)
-
-	return render_template('signup.html')
+	if session['error']:
+		return render_template('signup.html',error=session['error'])
+	else:
+		return render_template('signup.html'),200
 
 @sitemod.route('/dashboard')
-def dashboard():
+@login_required
+def dashboard(current_user):
 	if g.user:
 		usermail = session['user']
 		username = User.query.filter_by(email=usermail).first().name
@@ -70,7 +86,8 @@ def dashboard():
 		return redirect(url_for('site.signup'))
 
 @sitemod.route('/note',methods=['GET','POST'])
-def note():
+@login_required
+def note(current_user):
 	data = request.get_json('id')
 	id = int(data['id'])
 	note = Notes.query.get(id)
@@ -78,13 +95,15 @@ def note():
 		
 
 @sitemod.route('/logout')
-def logout():
-	session.pop('user',None)
+@login_required
+def logout(current_user):
+	session.pop(current_user,None)
 	
 	return redirect(url_for('site.signup'))
 
 @sitemod.route('/new_note',methods=['GET','POST'])
-def new_note():
+@login_required
+def new_note(current_user):
 	if request.method == 'POST':
 		if g.user:
 			title = request.form['note-title']
@@ -100,6 +119,7 @@ def new_note():
 	return redirect(url_for('site.dashboard'))
 
 @sitemod.route('/get_note/<id>',methods=['GET','POST'])
-def get_note(id):
+@login_required
+def get_note(current_user,id):
 	note = Notes.query.get(id)
 	return render_template('notepage.html')
